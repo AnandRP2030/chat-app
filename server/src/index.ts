23586760e5353cd3b8bfeba542eef042885d0b35
Anand R P <anandrp.sics@gmail.com>
2025-01-16 20:55:42 +0530

    feat: be-room logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,48 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-const allSockets: WebSocket[] = [];
+interface Room {
+  socket: WebSocket;
+  roomId: string;
+}
 
-wss.on("connection", (ws) => {
-  ws.on("error", console.error);
-  allSockets.push(ws);
-  // setInterval(() => {
-  //   ws.send("ITC price" + Math.random());
-  // }, 1000);
-  userCount++;
-  ws.send(`userCount:  ${userCount}`);
-  console.log(`user count: ${userCount}`)
-  ws.on("message", (message) => {
-    const msg = message.toString();
-    
-    allSockets.forEach((s) => {
-      s.send(msg)
-    })
-    
+let allSockets: Room[] = [];
+wss.on("connection", (socket: WebSocket) => {
+  socket.on("message", (message: string) => {
+    try {
+      const parsedMessage = JSON.parse(message.toString());
+      if (parsedMessage.type === "join") {
+        const previousMembersOnSameRoom = allSockets.filter(
+          (room: Room) => room.roomId === parsedMessage.payload.roomId
+        );
+
+        allSockets.push({
+          socket,
+          roomId: parsedMessage.payload.roomId,
+        });
+
+        previousMembersOnSameRoom.forEach((room: Room) => {
+          room.socket.send(`A new guy joined on ${parsedMessage.payload.roomId}`);
+        });
+
+        socket.send(
+          `You have successfully joined the room ${parsedMessage.payload.roomId}`
+        );
+      }
+
+      if (parsedMessage.type === "chat") {
+        const currentUserRooms = allSockets.filter(
+          (room: Room) => room.roomId === parsedMessage.payload.roomId
+        );
+        currentUserRooms.forEach((currRoom) => {
+          currRoom.socket.send(`${parsedMessage.payload.message}`);
+        });
+      }
+    } catch (error) {
+      console.error("Invalid json: ", message.toString(), error);
+    }
+  });
+  socket.on("close", () => {
+    socket.send("disconnected / server stopped");
   });
 });
