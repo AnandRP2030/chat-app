d8ed34763dbceafed84bf86961727ff2faa91ca5
Anand R P <anandrp.sics@gmail.com>
2025-01-16 16:07:38 +0530

    fe: tailwind config and basic UI , be: send msg to all users

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,24 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+const allSockets: WebSocket[] = [];
 
 wss.on("connection", (ws) => {
   ws.on("error", console.error);
-//   setInterval(() => {
-//     ws.send("ITC price" + Math.random());
-//   }, 1000);
-  ws.on("message", (e) => {
-    const msg = e.toString();
-    console.log('msg', msg)
-    if (msg == "ping") {
-        console.log('msg 2', msg)
-      ws.send("pong");
-    }else {
-        
-        ws.send("pong not working");
-    }
+  allSockets.push(ws);
+  // setInterval(() => {
+  //   ws.send("ITC price" + Math.random());
+  // }, 1000);
+  userCount++;
+  ws.send(`userCount:  ${userCount}`);
+  console.log(`user count: ${userCount}`)
+  ws.on("message", (message) => {
+    const msg = message.toString();
+    
+    allSockets.forEach((s) => {
+      s.send(msg)
+    })
     
   });
 });
